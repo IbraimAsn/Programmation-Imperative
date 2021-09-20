b6498a06c2611fbc5b65915009c1506e9574c1c8
Ibraim A <ibraim.asn@gmail.com>
2021-09-20 13:59:37 +0200

    complete exo3.c

--- exo4.c
+++ exo4.c
@@ -1,24 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-#define MAX_FILE_SIZE 2147483647 // also known as 2.147483647 GB
-
-int convert_chars_into_num(char c)
+int tells_if_digit(char c)
 {
     if((int)c >= 48 && (int)c <= 57)
-    {
-        return (int)c - 48;
-    }
-    return -1;
+        return 1;
+    return 0;
 }
 
-/*void allocates_memory_for_array(char *array_of_chars, int file_size)
+int get_amount_numbers(FILE *file_pointer)
 {
-    
-}*/
+    int counter_numbers = 0;
+    char previous_char, current_char;
+    while((current_char = fgetc(file_pointer)) != EOF)
+    {
+        if(tells_if_digit(current_char) == 1)
+        {
+            if(counter_numbers == 0)
+            {
+                counter_numbers++;
+            } 
+            else if(tells_if_digit(previous_char) != 1)
+            {
+                counter_numbers++;
+            }
+        }
+        previous_char = current_char;
+    }
+    return counter_numbers;
+}
+
+void add_numbers_to_arr(FILE *file_pointer, int *arr, int length_arr)
+{
+    char c;
+    int i = 0, sum = 0, checker = 0;
+    while((c = fgetc(file_pointer)) != EOF)
+    {
+        if(tells_if_digit(c))
+        {
+            if(checker == 0)
+            {
+                checker++;
+            }
+            sum = sum * 10 + (int)c - 48;
+        }
+        else if(checker != 0)
+        {
+            arr[i] = sum;
+            i++;
+            sum = 0;
+            checker = 0;
+        }
+    }
+    if(checker != 0)
+    {
+        arr[length_arr-1] = sum;
+    }
+}
+
+void displays_arr(int *arr, int length_arr)
+{
+    for(int i = 1; i < length_arr+1; i++)
+    {
+        if(i != 0 && i % 3 == 0 && i != length_arr)
+        {
+            printf("%d\n",arr[i-1]);
+        }
+        else
+        {
+            printf("%d\t",arr[i-1]);
+        }
+    }
+    printf("\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -28,13 +83,14 @@ int main(int argc, char *argv[])
         printf("error : could not open %s\n",argv[1]);
         exit(1);
     }
-    struct stat st;
-    stat(argv[1], &st);
-    off_t file_size = st.st_size;
-    char *array_of_chars;
-    //allocates_memory_for_array(array_of_chars,(int)file_size);
-    printf("size of %s : %d\n",argv[1],(int)file_size);
-    free(array_of_chars);
+    int length_arr = get_amount_numbers(file_pointer);
+    int *arr = malloc(sizeof(int)*length_arr);
+    fclose(file_pointer);
+    file_pointer = fopen(argv[1],"r");
+    add_numbers_to_arr(file_pointer,arr,length_arr);
+    printf("# of numbers : %d\n\n",length_arr);
+    displays_arr(arr,length_arr);
+    free(arr);
     fclose(file_pointer);
     return 0;
-}   
+}
